18d569faa46186b0448cc0dc42b475eecd2f079c
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-18 13:30:45 -0500

    fix homework, add calculator project

---
+++ Calculator/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // New scanner
+	    Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the calculator, What operation would you like to perform?");
+        System.out.println("1 for Addition");
+        System.out.println("2 for Subtraction");
+        System.out.println("3 for Multiplication");
+        System.out.println("4 for Division");
+        System.out.println("5 for Square Root");
+        System.out.println("6 to exit");
+
+        int option = scan.nextInt();
+
+        switch(option) {
+            case 1:
+                System.out.println("Please enter the first number");
+                int add1 = scan.nextInt();
+                System.out.println("Please enter the second number");
+                int add2 = scan.nextInt();
+                int addAnswer = add1 + add2;
+                System.out.println(addAnswer);
+                break;
+            case 2:
+                System.out.println("Please enter the first number");
+                int sub1 = scan.nextInt();
+                System.out.println("Please enter the second number");
+                int sub2 = scan.nextInt();
+                int subAnswer = sub1 - sub2;
+                System.out.println(subAnswer);
+                break;
+
+        }
+
+    }
+}
