37a1237573e1a251975510fc047cba501f8e6bba
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 16:14:32 -0500

    create triangle class

--- CreateShape/src/com/company/Triangle.java
+++ CreateShape/src/com/company/Triangle.java
@@ -1,4 +1,27 @@
 package com.company;
 
 public class Triangle {
+    private double base;
+    private double height;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double area() {
+        double triArea = (base * height) /2;
+        return triArea;
+    }
 }
