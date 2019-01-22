7e20051eaedc5d3ce52acb5bf328f895f0c17e65
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 15:27:31 -0500

    ask user input, validation complete for non numeric choices, need to add string validation

--- CreateShape/src/com/company/ShapeDemo.java
+++ CreateShape/src/com/company/ShapeDemo.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class ShapeDemo {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+        boolean error = false;
+
+        do {
+            System.out.println("What shape would you like to make?");
+            System.out.println("1 for Circle");
+            System.out.println("2 for Triangle");
+            System.out.println("3 for Square");
+            int option;
+
+            if(scan.hasNextInt()) {
+                option = scan.nextInt();
+                if(option == 1 || option == 2|| option == 3) {
+                    System.out.println("Success " + option);
+                    error = false;
+                } else {
+                    System.out.println("Please enter a vaild choice");
+                    error = true;
+                }
+            }
+
+
+        }while(error);
+
+
     }
 }
