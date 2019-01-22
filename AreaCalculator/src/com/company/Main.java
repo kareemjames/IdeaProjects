5bef893bc431bc222ed576b41a321a3f0ca4cfa7
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 14:09:05 -0500

    create area calc project

---
+++ AreaCalculator/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+//• Create a new project in IntelliJ named AreaCalculator using the Command Line App template
+//• Initialize your project as a Git repository and be sure to add and commit all changes as you work
+//• Rename your Main file to CalculatorDemo (right-click -> Refactor -> Rename...)
+//            • Create a new Java class file (File -> New -> Java Class) named Calculator
+//• Your Calculator should have (2) properties: width and height (with accessor methods)
+//• Your Calculator should have a method named calculateArea() which:
+//• Accepts width and height as parameters
+//• Calculates the area of the rectangle
+//• Returns the area
+//• In your CalculatorDemo, create an instance of Calculator and test your calculateArea() method
+//• Bonus: Modify your program so that your calculator has a shape property, and modify calculateArea() to accept shape, width, and height. Your method should then calculate the area of the shape depending on whether the shape parameter is “triangle” or “rectangle”, and should return the area
+
+    public static void main(String[] args) {
+
+        
+    }
+}
