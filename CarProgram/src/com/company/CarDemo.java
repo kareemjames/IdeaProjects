188f44d2dfe1ed389df36aa86decc5ec1029879e
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 13:46:10 -0500

    initial commit, create car program

---
+++ CarProgram/src/com/company/CarDemo.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class CarDemo {
+
+//    Create a new project in IntelliJ named CarProgram using the Command Line
+//    App template
+//    • Initialize your project as a Git repository and be sure to add and commit all changes as you work
+//    • Rename your CarDemo file to CarDemo (right-click -> Refactor -> Rename...)
+//    • Create a new Java class file (File -> New -> Java Class) named Car
+//    • Define your Car class with at least (3) instance variables such as color, make, model, etc., and create accessor methods for each variable
+//    • In your CarDemo class, write a program that instantiates a Car object named myCar, uses accessor methods to set each of its instance variables and then get their values, and finally prints out information about your vehicle
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
