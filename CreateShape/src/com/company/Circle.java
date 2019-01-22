0d8e4e049a042ac6280ec913874a01d33cecd1a3
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 15:41:50 -0500

    add logic for circle class

--- CreateShape/src/com/company/Circle.java
+++ CreateShape/src/com/company/Circle.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class Circle {
+    double radius;
+
+    private double area() {
+        double circleArea = Math.PI * radius * radius;
+        return circleArea;
+    }
 }
