421996d5ecdd3d9bd45adb5eb1c1ce4bb77ebb39
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 14:02:23 -0500

    create new car, tested successfully

--- CarProgram/src/com/company/CarDemo.java
+++ CarProgram/src/com/company/CarDemo.java
@@ -11,6 +11,17 @@ public class CarDemo {
 //    • In your CarDemo class, write a program that instantiates a Car object named myCar, uses accessor methods to set each of its instance variables and then get their values, and finally prints out information about your vehicle
 
     public static void main(String[] args) {
-	// write your code here
+
+        Car honda = new Car();
+
+        honda.setMake("Honda");
+        honda.setModel("Civic Si");
+        honda.setColor("Electron Blue Pearl");
+
+        System.out.println("My car make is " + honda.getMake());
+        System.out.println("My car model is " + honda.getModel());
+        System.out.println("My car color is " + honda.getColor());
+        honda.honkHorn();
+
     }
 }
