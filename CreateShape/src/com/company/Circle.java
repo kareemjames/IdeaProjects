faa21e901ce073b57e787886346a497878a7dc95
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-22 15:45:29 -0500

    add setter and getter for circle

--- CreateShape/src/com/company/Circle.java
+++ CreateShape/src/com/company/Circle.java
@@ -3,6 +3,14 @@ package com.company;
 public class Circle {
     double radius;
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     private double area() {
         double circleArea = Math.PI * radius * radius;
         return circleArea;
