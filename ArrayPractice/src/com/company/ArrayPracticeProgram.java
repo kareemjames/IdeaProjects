a106c1b381a440661152c2c74a819ac9b030eab0
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 14:30:14 -0500

    save input to temperature object, loop through objects and print ttemps to console

--- ArrayPractice/src/com/company/ArrayPracticeProgram.java
+++ ArrayPractice/src/com/company/ArrayPracticeProgram.java
@@ -14,45 +14,75 @@ public class ArrayPracticeProgram {
         double avg= 0;
         int count = 0;
 
+        // Temp Object conversion
+        // New object array
+        Temperature[] tempArray = new Temperature[7];
+
+        // Instantiate temp objects
+        for(int x = 0; x < tempArray.length; x++) {
+            tempArray[x] = new Temperature();
+        }
+
         Scanner scan = new Scanner(System.in);
         System.out.println("This program will collect information on daily average temps");
+
         System.out.println("Please enter the temp for Sunday");
-        dailyAverageTemp[0] = scan.nextInt();
+        //dailyAverageTemp[0] = scan.nextInt();
+        tempArray[0].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Monday");
-        dailyAverageTemp[1] = scan.nextInt();
+        //dailyAverageTemp[1] = scan.nextInt();
+        tempArray[1].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Tuesday");
-        dailyAverageTemp[2] = scan.nextInt();
+        //dailyAverageTemp[2] = scan.nextInt();
+        tempArray[2].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Wednesday");
-        dailyAverageTemp[3] = scan.nextInt();
+        //dailyAverageTemp[3] = scan.nextInt();
+        tempArray[3].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Thursday");
-        dailyAverageTemp[4] = scan.nextInt();
+        //dailyAverageTemp[4] = scan.nextInt();
+        tempArray[4].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Friday");
-        dailyAverageTemp[5] = scan.nextInt();
+        //dailyAverageTemp[5] = scan.nextInt();
+        tempArray[5].setTempInF(scan.nextInt());
+
         System.out.println("Please enter the temp for Saturday");
-        dailyAverageTemp[6] = scan.nextInt();
+        //dailyAverageTemp[6] = scan.nextInt();
+        tempArray[6].setTempInF(scan.nextInt());
+
         System.out.println("Thank you for this information, I will get right on it.");
 
-        for(i = 0; i < dailyAverageTemp.length; i++) {
-            System.out.println(dailyAverageTemp[(int)i]);
-            sum += dailyAverageTemp[(int)i];
+//        // Loop through dailyAverageTemp to display temp in F
+//        for(i = 0; i < dailyAverageTemp.length; i++) {
+//            System.out.println(dailyAverageTemp[(int)i]);
+//            sum += dailyAverageTemp[(int)i];
+//        }
+
+        // Loop through objects to display temps in F
+        for(Temperature g : tempArray) {
+            System.out.println(g.getTempInF());
         }
 
-        avg = sum / dailyAverageTemp.length;
-        System.out.println("The average temp for the week is " + avg);
-
-        for(double z : dailyAverageTemp) {
-            //System.out.println(z);
-            z = (z-32) * 5/9;
-            //System.out.println(z);
-            dailyAverageTempCelcius[count] = z;
-            count ++;
-        }
-
-        System.out.println("Your temp in Celcius is:");
-        //System.out.println(Arrays.toString(dailyAverageTempCelcius)); This is how to print an array
-
-        for(double y = 0; y < dailyAverageTempCelcius.length; y++) {
-            System.out.println(dailyAverageTempCelcius[(int) y]);
-        }
+//        avg = sum / dailyAverageTemp.length;
+//        System.out.println("The average temp for the week is " + avg);
+//
+//        for(double z : dailyAverageTemp) {
+//            //System.out.println(z);
+//            z = (z - 32) * 5 / 9;
+//            //System.out.println(z);
+//            dailyAverageTempCelcius[count] = z;
+//            count ++;
+//        }
+//
+//        System.out.println("Your temp in Celcius is:");
+//        //System.out.println(Arrays.toString(dailyAverageTempCelcius)); This is how to print an array
+//
+//        for(double y = 0; y < dailyAverageTempCelcius.length; y++) {
+//            System.out.println(dailyAverageTempCelcius[(int) y]);
+//        }
     }
 }
