41395b664ab14665cc91c7826b0c7f4930ea957a
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 18:01:43 -0500

    moved instantion of library, books and starter book outside of the do/while loop. I was getting a bug that user input was not being saved but it was actually being overwritten each time the loop reran. add and remove print statements to test if userinput was saved

--- LibraryProgram/src/com/company/LibraryDemo.java
+++ LibraryProgram/src/com/company/LibraryDemo.java
@@ -9,35 +9,53 @@ public class LibraryDemo {
         boolean isBrowsing = false;
         Scanner scan = new Scanner(System.in);
         int option;
+        int count = 1;
+
+        // Instantiate library
+        Library library = new Library();
+        // Instantiate books
+        for(int i = 0; i < library.books.length; i++) {
+            library.books[i] = new Book();
+        }
+        // Add starter book
+        library.books[0].setTitle("Cat In The Hat");
+        library.books[0].setAuthor("Dr. Suess");
+        library.books[0].setGenre("Childrens");
+        library.books[0].setNoOfPages(50);
 
         do {
-            // Instantiate library
-            Library library = new Library();
-            // Instantiate books
-            for(int i = 0; i < library.books.length; i++) {
-                library.books[i] = new Book();
-            }
-            // Add starter book
-            library.books[0].setTitle("Cat In The Hat");
-            library.books[0].setAuthor("Dr. Suess");
-            library.books[0].setGenre("Childrens");
-            library.books[0].setNoOfPages(50);
-
-
-
             System.out.println("Welcome to ETA Library! \nWhat would you like to do? \n 1 View number of books \n 2 Add new book \n 3 Exit program");
             option = scan.nextInt();
 
             switch(option) {
                 case 1:
                     System.out.println("You have " + library.numberOfBooksInLibrary() + " books in your library");
-                    System.out.println(library.books[0].printDetails());
+                    for(int i = 0; i < library.numberOfBooksInLibrary(); i++) {
+                        System.out.println(library.books[i].printDetails());
+                    }
                     isBrowsing = true;
                     break;
                 case 2:
+                    if(count < library.numberOfBooksInLibrary()) {
+                        System.out.println("count: " + count);
+                        System.out.println("Great! You have decided to add a book");
+                        System.out.println("Please provide the Title");
+                        library.books[count].setTitle(scan.next());
+                        System.out.println("Author");
+                        library.books[count].setAuthor(scan.next());
+                        System.out.println("Genre");
+                        library.books[count].setGenre(scan.next());
+                        System.out.println("Number of Pages");
+                        library.books[count].setNoOfPages(scan.nextInt());
 
-                    System.out.println("Great! You have decided to add a book \nPlease provide the title");
+                        System.out.println("Just added: " + library.books[count].getTitle());
+                        count++;
+                        isBrowsing = true;
 
+                    } else {
+                        System.out.println("The library is full, try again later");
+                        isBrowsing = true;
+                    }
                     break;
                 case 3:
                     System.exit(0);
