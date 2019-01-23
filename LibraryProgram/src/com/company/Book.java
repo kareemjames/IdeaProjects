afa938679d0b65c8a2573ce86760685b10eac5bf
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 15:21:21 -0500

    create Book class

--- LibraryProgram/src/com/company/Book.java
+++ LibraryProgram/src/com/company/Book.java
@@ -1,4 +1,45 @@
 package com.company;
 
 public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int noOfPages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getNoOfPages() {
+        return noOfPages;
+    }
+
+    public void setNoOfPages(int noOfPages) {
+        this.noOfPages = noOfPages;
+    }
+
+    public String printDetails() {
+        String details = title + " " + author + " " + genre + " " + noOfPages;
+        return details;
+    }
 }
