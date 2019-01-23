89143f3988171373a7166285f980213d797bd803
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 09:19:36 -0500

    add logic for square class

--- CreateShape/src/com/company/Square.java
+++ CreateShape/src/com/company/Square.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Square {
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double area() {
+        double squareArea = side * side;
+        return squareArea;
+    }
+
+    public double perimeter() {
+        double squarePerimeter = side * 4;
+        return squarePerimeter;
+    }
 }
