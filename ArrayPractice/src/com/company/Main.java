fa87a789e9b15b6567ae362edea3e59b1088c20c
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 10:55:02 -0500

    create daily temp array, user input is stored in array

--- ArrayPractice/src/com/company/Main.java
+++ ArrayPractice/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double[] dailyAverageTemp = new double[7];
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("This program will collect information on daily average temps");
+        System.out.println("Please enter the temp for Sunday");
+        dailyAverageTemp[0] = scan.nextInt();
+        System.out.println("Please enter the temp for Monday");
+        dailyAverageTemp[1] = scan.nextInt();
+        System.out.println("Please enter the temp for Tuesday");
+        dailyAverageTemp[2] = scan.nextInt();
+        System.out.println("Please enter the temp for Wednesday");
+        dailyAverageTemp[3] = scan.nextInt();
+        System.out.println("Please enter the temp for Thursday");
+        dailyAverageTemp[4] = scan.nextInt();
+        System.out.println("Please enter the temp for Friday");
+        dailyAverageTemp[5] = scan.nextInt();
+        System.out.println("Please enter the temp for Saturday");
+        dailyAverageTemp[6] = scan.nextInt();
+        System.out.println("Thank you for this information, I will get right on it.");
+        System.out.println(Arrays.toString(dailyAverageTemp));
     }
 }
