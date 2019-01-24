4a167d97791e7ae7a2a9d6c0160e98168b19b690
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 13:41:58 -0500

    add method to return contacts size

--- AddressBookProgram/src/com/company/AddyBook.java
+++ AddressBookProgram/src/com/company/AddyBook.java
@@ -4,5 +4,8 @@ import java.util.ArrayList;
 
 public class AddyBook {
     public ArrayList<Contact> contacts = new ArrayList<Contact>();
-    
+
+    public int numberOfContacts() {
+        return contacts.size();
+    }
 }
