75353bd53e9b6a9867bd819fe9fdbfbdbf5771f9
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 13:13:20 -0500

    initial commit, create contact class

--- AddressBookProgram/src/com/company/Contact.java
+++ AddressBookProgram/src/com/company/Contact.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class Contact {
+    private String name;
+    private String address;
+    private int phoneNumber;
+    private int birthDate;
+
+    public Contact(String name, String address, int phoneNumber, int birthDate) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
 }
