e53c9f35bfd7eb8ad14f9409fa12144a8ab38833
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-23 22:07:17 -0500

    initial commit

---
+++ HangmanProgram/src/com/company/HangmanDemo.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HangmanDemo {
+
+    public static void main(String[] args) {
+
+        boolean isPlaying = false;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Welcome to Hangman");
+
+        } while(isPlaying);
+
+
+
+    }
+}
