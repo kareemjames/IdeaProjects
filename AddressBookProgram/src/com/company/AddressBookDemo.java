87273a10596d8a3b023500771cf321244ac585d9
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 14:08:01 -0500

    add begininning logic for 1 1st prompt

--- AddressBookProgram/src/com/company/AddressBookDemo.java
+++ AddressBookProgram/src/com/company/AddressBookDemo.java
@@ -9,15 +9,23 @@ public class AddressBookDemo {
         Scanner scan = new Scanner(System.in);
         int option;
 
+        // instantiate addyBook
+        AddyBook addyBook = new AddyBook();
 
         do {
 
             System.out.println("Welcome to your book of contacts. What would you like to do?");
-            System.out.println("Press 1 to view current contacts \n 2 to add a contact \n 3 search by name \n 4 search by phone \n 5 delete contact \n 6 quit");
+            System.out.println("1 view current contacts \n2 add a contact \n3 search by name \n4 search by phone \n5 delete contact \n6 quit");
             option = scan.nextInt();
 
             switch(option) {
                 case 1:
+                    if(addyBook.numberOfContacts() == 0) {
+                        System.out.println("You have no contacts yet, please add one");
+                        areBrowsing = true;
+                    } else {
+                        return;
+                    }
                     break;
                 default:
                     break;
