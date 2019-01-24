7ba96c011786aba0d2a450905e1be77c8b697c4f
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 08:26:25 -0500

    convert random word to array of chars

--- HangmanProgram/src/com/company/HangmanDemo.java
+++ HangmanProgram/src/com/company/HangmanDemo.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,15 +14,25 @@ public class HangmanDemo {
 
         do {
             String[] words = {"hello", "bread", "fruit", "yellow"};
+            int guess = 0;
 
             // Randomizer to pick random word from array
             Random random = new Random();
             int index = random.nextInt(words.length);
             String randomWord = words[index];
-
             System.out.println(randomWord);
 
+            // Convert string to array of char
+            char[] chars = randomWord.toCharArray();
+            System.out.println(chars.length);
+
             System.out.println("Welcome to Hangman");
+            System.out.println("The length of the mystery word is " + randomWord.length());
+            System.out.println("Guess a letter and try to win before you get hung");
+            String letter = scan.next();
+
+
+
 
         } while(isPlaying);
 
