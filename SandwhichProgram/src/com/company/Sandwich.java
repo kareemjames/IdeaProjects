1ebccbdc92a0af4a4ae91cd27d840cda8f68a0c6
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 11:58:05 -0500

    create class constructor for sandwich maker, tested successfully

--- SandwhichProgram/src/com/company/Sandwich.java
+++ SandwhichProgram/src/com/company/Sandwich.java
@@ -1,4 +1,51 @@
 package com.company;
 
 public class Sandwich {
+    private String breadType;
+    private String proteinType;
+    private String cheeseType;
+    private boolean veggies;
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public String getProteinType() {
+        return proteinType;
+    }
+
+    public void setProteinType(String proteinType) {
+        this.proteinType = proteinType;
+    }
+
+    public String getCheeseType() {
+        return cheeseType;
+    }
+
+    public void setCheeseType(String cheeseType) {
+        this.cheeseType = cheeseType;
+    }
+
+    public boolean isVeggies() {
+        return veggies;
+    }
+
+    public void setVeggies(boolean veggies) {
+        this.veggies = veggies;
+    }
+
+    public void describeSandwich() {
+        System.out.println(breadType + " " + proteinType + " " + cheeseType + " " + veggies);
+    }
+
+    public Sandwich(String breadType, String proteinType, String cheeseType, boolean veggies) {
+        this.breadType = breadType;
+        this.proteinType = proteinType;
+        this.cheeseType = cheeseType;
+        this.veggies = veggies;
+    }
 }
---
+++ SandwhichProgram/src/com/company/SandwichMaker.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class SandwichMaker {
+
+    public static void main(String[] args) {
+
+        Sandwich pbj = new Sandwich("whole wheat", "peanut butter", "none", false);
+
+        pbj.describeSandwich();
+    }
+}
