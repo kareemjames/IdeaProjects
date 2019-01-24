ae8eede4891b99b1e8ec81e39cdb953b5619c782
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-24 13:57:36 -0500

    add switch outline and initial user prompt

--- AddressBookProgram/src/com/company/AddressBookDemo.java
+++ AddressBookProgram/src/com/company/AddressBookDemo.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class AddressBookDemo {
 
     public static void main(String[] args) {
+        boolean areBrowsing = false;
+        Scanner scan = new Scanner(System.in);
+        int option;
 
+
+        do {
+
+            System.out.println("Welcome to your book of contacts. What would you like to do?");
+            System.out.println("Press 1 to view current contacts \n 2 to add a contact \n 3 search by name \n 4 search by phone \n 5 delete contact \n 6 quit");
+            option = scan.nextInt();
+
+            switch(option) {
+                case 1:
+                    break;
+                default:
+                    break;
+
+            }
+
+        } while(areBrowsing);
     }
 }
