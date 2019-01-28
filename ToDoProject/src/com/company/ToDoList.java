b0cff3e904eeba2dc8004be93e3954994c1f402f
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-28 18:56:20 -0500

    add constructor, getlists method, getters and setters

--- ToDoProject/src/com/company/ToDoList.java
+++ ToDoProject/src/com/company/ToDoList.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 
 public class ToDoList {
 
+    private String name;
     private ArrayList<ToDoList> list = new ArrayList<ToDoList>();
 
+    public ToDoList(String name) {
+        this.name = name;
+    }
 
+    private void getListOfToDoLists() {
+        for(int i =0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
