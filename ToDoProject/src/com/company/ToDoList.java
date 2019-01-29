fd227b93a7440ea635bb04458b7ba314afc8bb19
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-28 21:10:36 -0500

    able to add new todo list and print out lists to user

--- ToDoProject/src/com/company/ToDoList.java
+++ ToDoProject/src/com/company/ToDoList.java
@@ -7,15 +7,26 @@ public class ToDoList {
     private String name;
     private ArrayList<ToDoList> list = new ArrayList<ToDoList>();
 
+    public ToDoList() {
+
+    }
+
     public ToDoList(String name) {
         this.name = name;
     }
 
-    private void getListOfToDoLists() {
-        for(int i =0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+    public void getListOfToDoLists() {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).name);
         }
+        System.out.println("You have "+ list.size() + " list(s)");
     }
+
+   public void addTodoList(ToDoList x) {
+        list.add(x);
+       System.out.println("You have successfully added the " + x.name + " todo list");
+       getListOfToDoLists();
+   }
 
     public String getName() {
         return name;
--- ToDoProject/src/com/company/ToDoListLogic.java
+++ ToDoProject/src/com/company/ToDoListLogic.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ToDoListLogic {
     Scanner scan = new Scanner(System.in);
     String userInput;
+    ToDoList toDoList = new ToDoList();
 
     ToDoListLogic() {
 
@@ -24,5 +25,20 @@ public class ToDoListLogic {
         System.out.println("Welcome to your ToDoList! \nType create - Create Todo List \nview - View Todos \nexit - Exit");
         userInput = stringInput(userInput);
         System.out.println(userInput);
+
+        if(userInput.equals("create")) {
+            createToDo();
+        }
     }
+
+    public void createToDo() {
+        System.out.println("Please add the name of your Todo List");
+
+        userInput = stringInput(userInput);
+        ToDoList toDoListN = new ToDoList(userInput);
+        toDoList.addTodoList(toDoListN);
+
+    }
+
+
 }
