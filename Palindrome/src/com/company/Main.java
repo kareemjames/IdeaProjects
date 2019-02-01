bfc199acb7fe8935f503282858c39beb0d9c2530
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-02-01 08:05:03 -0500

    finished palindrome

--- Palindrome/src/com/company/Main.java
+++ Palindrome/src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        isPalindrome("Was it a car or a cat I saw");
+        isPalindrome("racecar");
+        System.out.println(isPalindromeNoSpaceOrSpecial("Was it a car or a cat I saw?"));
 
     }
 
@@ -15,12 +16,23 @@ public class Main {
         for (int i = x.length() - 1; i >= 0; i--) {
             reverse = reverse + x.charAt(i);
         }
+        System.out.println(reverse);
 
-        if(x.equalsIgnoreCase(reverse)) {
+        if(x.equals(reverse)) {
             System.out.println("This is a Palindrome");
         } else {
             System.out.println("This is not a Palindrome");
         }
 
+    }
+
+    public static boolean isPalindromeNoSpaceOrSpecial(String x) {
+
+        String stripedX = x.replaceAll("[^a-zA-z0-9]", "");
+
+        StringBuilder stringBuilder = new StringBuilder(stripedX);
+        stringBuilder.reverse();
+
+        return stripedX.equalsIgnoreCase(stringBuilder.toString());
     }
 }
